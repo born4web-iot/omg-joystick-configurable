cb88d70c138fc714b54e297d46aa1e217e3c481a
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-09-04 22:52:11 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -5,6 +5,41 @@ def joystickMovingPoint():
     led.plot(x_pos, y_pos)
     basic.pause(100)
     led.unplot(x_pos, y_pos)
+def initialCOnfig():
+    global x_pos, y_pos, BTN_PAUSE, RADIO_GROUP, CONFIG_PARAM
+    x_pos = 2
+    y_pos = 2
+    pins.set_pull(DigitalPin.P13, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P14, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P15, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P16, PinPullMode.PULL_UP)
+    BTN_PAUSE = 200
+    RADIO_GROUP = 5
+    radio.set_group(RADIO_GROUP)
+    CONFIG_PARAM = 0
+
+def on_button_pressed_a():
+    global RADIO_GROUP
+    if CONFIG_PARAM == 1:
+        RADIO_GROUP += -1
+        if RADIO_GROUP < 0:
+            RADIO_GROUP = 0
+        showRadioGroup()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def showRadioGroup():
+    basic.clear_screen()
+    basic.show_number(RADIO_GROUP)
+
+def on_button_pressed_b():
+    global RADIO_GROUP
+    if CONFIG_PARAM == 1:
+        RADIO_GROUP += 1
+        if RADIO_GROUP > 255:
+            RADIO_GROUP = 255
+        showRadioGroup()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 def pullButtonsShowIcons():
     if pins.digital_read_pin(DigitalPin.P13) == 0:
         basic.show_leds("""
@@ -16,6 +51,7 @@ def pullButtonsShowIcons():
             """)
         basic.pause(BTN_PAUSE)
         basic.clear_screen()
+        radio.send_string("LEFT")
     elif pins.digital_read_pin(DigitalPin.P14) == 0:
         basic.show_leds("""
             . . # . .
@@ -26,6 +62,7 @@ def pullButtonsShowIcons():
             """)
         basic.pause(BTN_PAUSE)
         basic.clear_screen()
+        radio.send_string("UP")
     elif pins.digital_read_pin(DigitalPin.P15) == 0:
         basic.show_leds("""
             . . . . .
@@ -36,6 +73,7 @@ def pullButtonsShowIcons():
             """)
         basic.pause(BTN_PAUSE)
         basic.clear_screen()
+        radio.send_string("RIGHT")
     elif pins.digital_read_pin(DigitalPin.P16) == 0:
         basic.show_leds("""
             . . . . .
@@ -46,21 +84,34 @@ def pullButtonsShowIcons():
             """)
         basic.pause(BTN_PAUSE)
         basic.clear_screen()
+        radio.send_string("DOWN")
+
+def on_logo_pressed():
+    global CONFIG_PARAM
+    CONFIG_PARAM += 1
+    if CONFIG_PARAM > 1:
+        CONFIG_PARAM = 0
+        basic.clear_screen()
+    if CONFIG_PARAM == 1:
+        basic.show_string("G")
+        basic.pause(1000)
+        showRadioGroup()
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+CONFIG_PARAM = 0
+RADIO_GROUP = 0
 BTN_PAUSE = 0
 y_pos = 0
 x_pos = 0
 basic.show_icon(IconNames.SMALL_DIAMOND)
 basic.pause(2000)
 basic.clear_screen()
-x_pos = 2
-y_pos = 2
-pins.set_pull(DigitalPin.P13, PinPullMode.PULL_UP)
-pins.set_pull(DigitalPin.P14, PinPullMode.PULL_UP)
-pins.set_pull(DigitalPin.P15, PinPullMode.PULL_UP)
-pins.set_pull(DigitalPin.P16, PinPullMode.PULL_UP)
-BTN_PAUSE = 200
+initialCOnfig()
 
 def on_forever():
     joystickMovingPoint()
     pullButtonsShowIcons()
+    basic.pause(100)
+    radio.send_value("x_pos", x_pos)
+    radio.send_value("y_pos", y_pos)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,56 @@
-function joystickMovingPoint () {
+function joystickMovingPoint() {
+    
     x_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 4, 0))
     y_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 4, 0))
     led.plot(x_pos, y_pos)
     basic.pause(100)
     led.unplot(x_pos, y_pos)
 }
-function pullButtonsShowIcons () {
+
+function initialCOnfig() {
+    
+    x_pos = 2
+    y_pos = 2
+    pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+    BTN_PAUSE = 200
+    RADIO_GROUP = 5
+    radio.setGroup(RADIO_GROUP)
+    CONFIG_PARAM = 0
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (CONFIG_PARAM == 1) {
+        RADIO_GROUP += -1
+        if (RADIO_GROUP < 0) {
+            RADIO_GROUP = 0
+        }
+        
+        showRadioGroup()
+    }
+    
+})
+function showRadioGroup() {
+    basic.clearScreen()
+    basic.showNumber(RADIO_GROUP)
+}
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (CONFIG_PARAM == 1) {
+        RADIO_GROUP += 1
+        if (RADIO_GROUP > 255) {
+            RADIO_GROUP = 255
+        }
+        
+        showRadioGroup()
+    }
+    
+})
+function pullButtonsShowIcons() {
     if (pins.digitalReadPin(DigitalPin.P13) == 0) {
         basic.showLeds(`
             . . . . .
@@ -16,6 +61,7 @@ function pullButtonsShowIcons () {
             `)
         basic.pause(BTN_PAUSE)
         basic.clearScreen()
+        radio.sendString("LEFT")
     } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
         basic.showLeds(`
             . . # . .
@@ -26,6 +72,7 @@ function pullButtonsShowIcons () {
             `)
         basic.pause(BTN_PAUSE)
         basic.clearScreen()
+        radio.sendString("UP")
     } else if (pins.digitalReadPin(DigitalPin.P15) == 0) {
         basic.showLeds(`
             . . . . .
@@ -36,6 +83,7 @@ function pullButtonsShowIcons () {
             `)
         basic.pause(BTN_PAUSE)
         basic.clearScreen()
+        radio.sendString("RIGHT")
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
         basic.showLeds(`
             . . . . .
@@ -46,22 +94,39 @@ function pullButtonsShowIcons () {
             `)
         basic.pause(BTN_PAUSE)
         basic.clearScreen()
+        radio.sendString("DOWN")
     }
+    
 }
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    
+    CONFIG_PARAM += 1
+    if (CONFIG_PARAM > 1) {
+        CONFIG_PARAM = 0
+        basic.clearScreen()
+    }
+    
+    if (CONFIG_PARAM == 1) {
+        basic.showString("G")
+        basic.pause(1000)
+        showRadioGroup()
+    }
+    
+})
+let CONFIG_PARAM = 0
+let RADIO_GROUP = 0
 let BTN_PAUSE = 0
 let y_pos = 0
 let x_pos = 0
 basic.showIcon(IconNames.SmallDiamond)
 basic.pause(2000)
 basic.clearScreen()
-x_pos = 2
-y_pos = 2
-pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-BTN_PAUSE = 200
-basic.forever(function () {
+initialCOnfig()
+basic.forever(function on_forever() {
     joystickMovingPoint()
     pullButtonsShowIcons()
+    basic.pause(100)
+    radio.sendValue("x_pos", x_pos)
+    radio.sendValue("y_pos", y_pos)
 })
