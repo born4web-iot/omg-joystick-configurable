c07664151f7af839620f271b4f0c727695c11c22
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-09-04 23:20:43 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,79 @@
-function joystickMovingPoint() {
-    
-    x_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, 4, 0))
-    y_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, 4, 0))
+function joystickMovingPoint () {
+    x_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P2), 0, 1023, X_MAX, 0))
+    y_pos = Math.round(Math.map(pins.analogReadPin(AnalogPin.P1), 0, 1023, Y_MAX, 0))
     led.plot(x_pos, y_pos)
     basic.pause(100)
     led.unplot(x_pos, y_pos)
 }
-
-function initialCOnfig() {
-    
-    x_pos = 2
-    y_pos = 2
+function initialCOnfig () {
+    RADIO_GROUP = 5
+    CONFIG_PARAM = 0
+    X_MAX = 4
+    Y_MAX = 4
+    BTN_PAUSE = 200
     pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-    BTN_PAUSE = 200
-    RADIO_GROUP = 5
+    x_pos = 2
+    y_pos = 2
     radio.setGroup(RADIO_GROUP)
-    CONFIG_PARAM = 0
 }
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (CONFIG_PARAM == 1) {
         RADIO_GROUP += -1
         if (RADIO_GROUP < 0) {
             RADIO_GROUP = 0
         }
-        
         showRadioGroup()
     }
-    
+    if (CONFIG_PARAM == 2) {
+        X_MAX += -1
+        if (X_MAX < 0) {
+            RADIO_GROUP = 0
+        }
+        showMaxX()
+    }
+    if (CONFIG_PARAM == 3) {
+        Y_MAX += -1
+        if (X_MAX < 0) {
+            Y_MAX = 0
+        }
+        showMaxY()
+    }
 })
-function showRadioGroup() {
+function showRadioGroup () {
     basic.clearScreen()
     basic.showNumber(RADIO_GROUP)
 }
-
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+function showMaxX () {
+    basic.clearScreen()
+    basic.showNumber(X_MAX)
+}
+input.onButtonPressed(Button.B, function () {
     if (CONFIG_PARAM == 1) {
         RADIO_GROUP += 1
         if (RADIO_GROUP > 255) {
             RADIO_GROUP = 255
         }
-        
         showRadioGroup()
     }
-    
+    if (CONFIG_PARAM == 2) {
+        X_MAX += 1
+        if (X_MAX > 255) {
+            X_MAX = 255
+        }
+        showMaxX()
+    }
+    if (CONFIG_PARAM == 3) {
+        Y_MAX += 1
+        if (Y_MAX > 255) {
+            Y_MAX = 255
+        }
+        showMaxY()
+    }
 })
-function pullButtonsShowIcons() {
+function pullButtonsShowIcons () {
     if (pins.digitalReadPin(DigitalPin.P13) == 0) {
         basic.showLeds(`
             . . . . .
@@ -96,34 +119,45 @@ function pullButtonsShowIcons() {
         basic.clearScreen()
         radio.sendString("DOWN")
     }
-    
 }
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
-    
+function showMaxY () {
+    basic.clearScreen()
+    basic.showNumber(Y_MAX)
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     CONFIG_PARAM += 1
-    if (CONFIG_PARAM > 1) {
+    if (CONFIG_PARAM > 3) {
         CONFIG_PARAM = 0
         basic.clearScreen()
     }
-    
     if (CONFIG_PARAM == 1) {
         basic.showString("G")
-        basic.pause(1000)
+        basic.pause(500)
         showRadioGroup()
     }
-    
+    if (CONFIG_PARAM == 2) {
+        basic.showString("X")
+        basic.pause(500)
+        showMaxX()
+    }
+    if (CONFIG_PARAM == 3) {
+        basic.showString("Y")
+        basic.pause(500)
+        showMaxY()
+    }
 })
+let BTN_PAUSE = 0
 let CONFIG_PARAM = 0
 let RADIO_GROUP = 0
-let BTN_PAUSE = 0
+let Y_MAX = 0
 let y_pos = 0
+let X_MAX = 0
 let x_pos = 0
 basic.showIcon(IconNames.SmallDiamond)
 basic.pause(2000)
 basic.clearScreen()
 initialCOnfig()
-basic.forever(function on_forever() {
+basic.forever(function () {
     joystickMovingPoint()
     pullButtonsShowIcons()
     basic.pause(100)
